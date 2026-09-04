59e20ce3080de5340688b23f50a001ba710be293
David Rojas <davixxsc15@gmail.com>
2026-09-04 17:47:11 -0500

    Hola desde sprint 2

--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -1,4 +1,5 @@
 package pe.edu.tecsup.lab03.entities;
 
 public class StudentEntity {
+    String v = "Jola";
 }
--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -1,4 +1,5 @@
 package pe.edu.tecsup.lab03.repositories;
 
 public class StudentRepository {
+    float c= 1.2F;
 }
